b649d626f46deeba2aaac0b5db87ea33706a5b84
joaomiguelgsn-create <joaomiguelgsn@gmail.com>
2026-04-30 11:37:46 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Cole aqui os dados do seu projeto Supabase
 // Project Settings > API
-window.JM_SUPABASE_URL = 'COLE_AQUI_A_URL_DO_SUPABASE';
-window.JM_SUPABASE_ANON_KEY = 'COLE_AQUI_A_ANON_KEY_DO_SUPABASE';
+window.JM_SUPABASE_URL = 'https://ubbhbrfvsiiyenbqlaqs.supabase.co';
+window.JM_SUPABASE_ANON_KEY = 'sb_publishable_LlVC6koUlpN2gRdwF2-KZw_ZIt-IdLY';
