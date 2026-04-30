3f4e802a604794946b6b6d4dba1d47d43f0cda9b
joaomiguelgsn-create <joaomiguelgsn@gmail.com>
2026-04-30 14:28:29 -0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Cole aqui os dados do seu projeto Supabase
-// Project Settings > API
-window.JM_SUPABASE_URL = 'https://ubbhbrfvsiiyenbqlaqs.supabase.co';
-window.JM_SUPABASE_ANON_KEY = 'sb_publishable_LlVC6koUlpN2gRdwF2-KZw_ZIt-IdLY';
